b0e3f04d8a0647109e84094c349674a4763925e4
stormning@163.com <stormning@163.com>
2013-09-05 17:21:34 +0800

    LoginOutController

---
+++ slyak-user/src/main/java/com/slyak/user/web/LoginoutController.java
@@ -0,0 +1,45 @@
+package com.slyak.user.web;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.shiro.authc.AuthenticationException;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.slyak.user.util.LoginUserHelper;
+
+@Controller
+public class LoginoutController {
+	
+	@RequestMapping(value="/doLogin",method=RequestMethod.POST)
+	public String doLogin(String username,String password,boolean rememberMe){
+		try{
+			LoginUserHelper.login(username, password, rememberMe);
+		}  catch (AuthenticationException ae){
+			//do noting
+		} 
+		return "redirect:/";
+	}
+	
+	@RequestMapping(value="/doAjaxLogin",method=RequestMethod.POST)
+	@ResponseBody
+	public Map<String,String> doAjaxLogin(String username,String password,boolean rememberMe){
+		Map<String,String> result = new HashMap<String, String>();
+		try{
+			LoginUserHelper.login(username, password, rememberMe);
+			result.put("success", "1");
+		}  catch (AuthenticationException ae){
+			result.put("success", "0");
+		} 
+		return result;
+	}
+
+	@RequestMapping("/logout")
+	public String logout(){
+		LoginUserHelper.logout();
+		return "redirect:/";
+	}
+}
